8b3c14feb75007c09111889c0c9d10327f513bd7
Maxim Miroshnichenko <max@miroshnichenko.org>
2026-09-01 12:25:43 +0300

    SL GC

--- StreamLoop/StreamLoop_GC.class.php
+++ StreamLoop/StreamLoop_GC.class.php
@@ -2,10 +2,11 @@
 class StreamLoop_GC extends StreamLoop_Timer_Abstract {
 
     public function __construct(StreamLoop $loop) {
-        $timerID = 100; // special timer ID for GC, будет -100 внутри
-        $timeout = 3600; // every hour
-
-        parent::__construct($loop, $timerID, $timeout);
+        parent::__construct(
+            $loop,
+            100, // special timer ID for GC, будет -100 внутри
+            3600 // every hour
+        );
     }
 
     protected function _onTimer($tsSelect) {
