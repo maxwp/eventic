d8f80c63c75638b50bd5fdc6c00951df882d357c
Maxim Miroshnichenko <max@miroshnichenko.org>
2026-09-22 13:33:42 +0300

    new SuperDebug

--- SuperVisor/SuperDebug.class.php
+++ SuperVisor/SuperDebug.class.php
@@ -2,7 +2,21 @@
 class SuperDebug extends EE_Content_Abstract_Cli {
 
     public function process() {
-        $this->print_r(SuperVisor::Get()->getConfigArray());
+        $superArray = SuperVisor::Get()->getConfigArray();
+
+        # debug:start
+        $this->print_r($superArray);
+        # debug:end
+
+        foreach ($superArray as $key => $a) {
+            $this->print_t($key);
+            $this->print_t($a['className']);
+            $this->print_n();
+            
+            $this->print_t(json_encode($a['argumentArray']));
+            $this->print_n();
+            $this->print_n();
+        }
     }
 
 }
